5229d5b038530e08e9b4289488b58ca7e3126aae
manoletta <manondarget.pro@gmail.com>
2019-12-15 12:05:36 +0100

    Ajout des formations dans la BD

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,29 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Formation;
+
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+
+      //Formations
+        $DU = new Formation();
+        $DU->setNomLong("Diplôme Universitaire en Technologies de l'Information et de la Communication");
+        $DU->setNomCourt("DU TIC");
+        $manager->persist($DU);
+
+        $DUT = new Formation();
+        $DUT->setNomLong("Diplôme Universitaire Technologique Informatique");
+        $DUT->setNomCourt("DUT Informatique");
+        $manager->persist($DUT);
+
+        $LP = new Formation();
+        $LP->setNomLong("Licence Professionnelle Multimédia");
+        $LP->setNomCourt("LP Multimédia");
+        $manager->persist($LP);
 
         $manager->flush();
     }
